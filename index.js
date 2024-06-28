52f4bf8e673df83d7af0cd8f4272aaa2ca2fa594
fels123 <kelengulienakhro@gmail.com>
2024-06-28 22:23:44 +0530

    moving elements inside an array using splice method

--- index.js
+++ index.js
@@ -18,14 +18,35 @@
 // }
 
 //Except excluding elements from an array
-const numbers = [1, 2, 3, 4, 1, 1];
-const output = except(numbers, [1]);
+// const numbers = [1, 2, 3, 4, 1, 1];
+// const output = except(numbers, [1]);
 
-console.log(output);
+// console.log(output);
 
-function except(array, excluded) {
-  const output = [];
-  for (let element of array)
-    if (!excluded.includes(element)) output.push(element);
+// function except(array, excluded) {
+//   const output = [];
+//   for (let element of array)
+//     if (!excluded.includes(element)) output.push(element);
+//   return output;
+// }
+
+//moving elements in an array
+const numbers = [1, 2, 3, 4];
+
+const output = move(numbers, 0, 5);
+console.log(output); // Output: [2, 3, 4, 1]
+
+function move(array, index, offset) {
+  const newPosition = index + offset;
+
+  // Check if newPosition is out of bounds
+  if (newPosition > array.length || newPosition < 0) {
+    console.error("Invalid Offset");
+    return;
+  }
+
+  const output = [...array]; // Create a copy of the original array
+  const element = output.splice(index, 1)[0]; // Remove the element at index
+  output.splice(newPosition, 0, element); // Insert the element at newPosition
   return output;
 }
