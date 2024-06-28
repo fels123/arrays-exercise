40f8718dd3514d639d504045e463ff0b18e08707
fels123 <kelengulienakhro@gmail.com>
2024-06-28 21:53:16 +0530

    includes method

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // array from range
-const numbers = arrayFromRange(1, 4);
-console.log(numbers);
+// const numbers = arrayFromRange(1, 4);
+// console.log(numbers);
 
-function arrayFromRange(min, max) {
-  const output = [];
-  for (let i = min; i <= max; i++) output.push(i);
-  return output;
+// function arrayFromRange(min, max) {
+//   const output = [];
+//   for (let i = min; i <= max; i++) output.push(i);
+//   return output;
+// }
+
+//includes method
+const numbers = [1, 2, 3, 4];
+console.log(includes(numbers, 5));
+
+function includes(array, searchElement) {
+  for (let element of array) if (element === searchElement) return true;
+  return false;
 }
