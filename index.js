448c08eb86dee15ce59e0a5ffc60ddef34e8c6f8
fels123 <kelengulienakhro@gmail.com>
2024-06-28 21:58:07 +0530

    excluding elements from an array

--- index.js
+++ index.js
@@ -9,10 +9,23 @@
 // }
 
 //includes method
-const numbers = [1, 2, 3, 4];
-console.log(includes(numbers, 5));
+// const numbers = [1, 2, 3, 4];
+// console.log(includes(numbers, 5));
 
-function includes(array, searchElement) {
-  for (let element of array) if (element === searchElement) return true;
-  return false;
+// function includes(array, searchElement) {
+//   for (let element of array) if (element === searchElement) return true;
+//   return false;
+// }
+
+//Except excluding elements from an array
+const numbers = [1, 2, 3, 4, 1, 1];
+const output = except(numbers, [1]);
+
+console.log(output);
+
+function except(array, excluded) {
+  const output = [];
+  for (let element of array)
+    if (!excluded.includes(element)) output.push(element);
+  return output;
 }
