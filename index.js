3aacd18f35314d49364897d8f3bb425a9a2a42e2
fels123 <kelengulienakhro@gmail.com>
2024-06-28 22:32:06 +0530

    reduce method

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 //   for (let i = min; i <= max; i++) output.push(i);
 //   return output;
 // }
-
+// *********************************************************************
 //includes method
 // const numbers = [1, 2, 3, 4];
 // console.log(includes(numbers, 5));
@@ -16,7 +16,7 @@
 //   for (let element of array) if (element === searchElement) return true;
 //   return false;
 // }
-
+// *********************************************************
 //Except excluding elements from an array
 // const numbers = [1, 2, 3, 4, 1, 1];
 // const output = except(numbers, [1]);
@@ -29,24 +29,41 @@
 //     if (!excluded.includes(element)) output.push(element);
 //   return output;
 // }
-
+// *************************************************************************
 //moving elements in an array
-const numbers = [1, 2, 3, 4];
+// const numbers = [1, 2, 3, 4];
 
-const output = move(numbers, 0, 5);
-console.log(output); // Output: [2, 3, 4, 1]
+// const output = move(numbers, 0, 5);
+// console.log(output); // Output: [2, 3, 4, 1]
 
-function move(array, index, offset) {
-  const newPosition = index + offset;
+// function move(array, index, offset) {
+//   const newPosition = index + offset;
 
-  // Check if newPosition is out of bounds
-  if (newPosition > array.length || newPosition < 0) {
-    console.error("Invalid Offset");
-    return;
-  }
+//   // Check if newPosition is out of bounds
+//   if (newPosition > array.length || newPosition < 0) {
+//     console.error("Invalid Offset");
+//     return;
+//   }
 
-  const output = [...array]; // Create a copy of the original array
-  const element = output.splice(index, 1)[0]; // Remove the element at index
-  output.splice(newPosition, 0, element); // Insert the element at newPosition
-  return output;
+//   const output = [...array]; // Create a copy of the original array
+//   const element = output.splice(index, 1)[0]; // Remove the element at index
+//   output.splice(newPosition, 0, element); // Insert the element at newPosition
+//   return output;
+// }
+// *************************************************************************
+//count occurances using reduce method
+const numbers = [1, 2, 3, 1, 4];
+const count = countOccurances(numbers, 1);
+console.log(count);
+function countOccurances(array, searchElement) {
+  //   let count = 0;
+  //   for (let element of array) if (element === searchElement) count++;
+  //   return count;
+  return array.reduce((accumulator, current) => {
+    const occurance = current === searchElement ? 1 : 0;
+    console.log(accumulator, current, searchElement);
+    return accumulator + occurance;
+  }, 0);
 }
+
+// ****************************************************************
